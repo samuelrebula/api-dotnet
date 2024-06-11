f35e16f931673c8b7f213ea519df1066fae24862
samuel <rebuuula@gmail.com>
2024-06-10 22:17:20 -0300

    creating the entity class

---
+++ api dotnet/Entities/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace api_dotnet.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+
+    }
+}
