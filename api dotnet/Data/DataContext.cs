e308ba50c67f9564585f3514050b861a4bf75199
samuel <rebuuula@gmail.com>
2024-06-13 00:28:38 -0300

    Adding Entity Framework, SQL Server Database and Implementing Code First Migrations

---
+++ api dotnet/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using api_dotnet.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_dotnet.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
